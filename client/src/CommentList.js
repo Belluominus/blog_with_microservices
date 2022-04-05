649905b8011c37d3651d5b2b5cd60377478302c4
Belluominus <jpscbelluominus@gmail.com>
2022-04-05 14:42:29 -0300

    finishing and docker

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 function CommentList({ comments }) {
     const renderComments = Object.values(comments).map(comment => {
+        let content;
+
+        if (comment.status === 'approved'){
+            content = comment.content;
+        }
+        if (comment.status === 'pending'){
+            content = 'This comment is awaiting moderation';
+        } 
+        if (comment.status === 'rejected'){
+            content = 'This comment has been rejected';
+        }
+
         return <li key={comment.id}>
-            {comment.content}
+            {content}
         </li>
     });
 
--- moderation/index.js
+++ moderation/index.js
@@ -5,26 +5,41 @@ import axios from 'axios';
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', async (req, res) => {
-    const { type, data }= req.body;
-
+const handleEvent = async (type, data) => {
     if (type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
-
+    
         await axios.post('http://localhost:4005/events', {
-            type: "CommentUpdated",
+            type: "CommentModerated",
             data: {
-                id: data.commentId, 
+                id: data.id, 
                 content: data.content,
-                postId: data.id,
+                postId: data.postId,
                 status
             }
         });
     };
+};
+
+app.post('/events', async (req, res) => {
+    const { type, data }= req.body;
+
+    handleEvent( type, data )
 
     res.send({});
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
     console.log('listening on 4003')
+    try {
+        const {data} = await axios.get("http://localhost:4005/events");
+     
+        for (let event of data) {
+          console.log("Processing event:", event.type);
+     
+          handleEvent(event.type, event.data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
 });
