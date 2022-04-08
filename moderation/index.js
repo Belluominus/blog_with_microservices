2fae3ba89bade062f1fbdd02f81c6a85a7fd0028
Belluominus <jpscbelluominus@gmail.com>
2022-04-08 16:43:22 -0300

    kube

--- moderation/index.js
+++ moderation/index.js
@@ -9,7 +9,7 @@ const handleEvent = async (type, data) => {
     if (type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
     
-        await axios.post('http://localhost:4005/events', {
+        await axios.post('http://event-bus-srv:4005/events', {
             type: "CommentModerated",
             data: {
                 id: data.id, 
@@ -32,7 +32,7 @@ app.post('/events', async (req, res) => {
 app.listen(4003, async () => {
     console.log('listening on 4003')
     try {
-        const {data} = await axios.get("http://localhost:4005/events");
+        const {data} = await axios.get("http://event-bus-srv:4005/events");
      
         for (let event of data) {
           console.log("Processing event:", event.type);
